ec1f2c2776a0cb2217e8d3b035a5519c98edabf0
Joey Bertschler <joey.bertschler@gmail.com>
2021-08-16 17:10:49 +0200

    wip

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require ('express')
+const cors = require('cors')
+
+const server = express()
+
+const PORT = process.env.PORT || 9000 //9000 = fallback if env missing
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/api/hello', (req,res)=>{
+    res.json({message: 'api is working'})
+})
+
+//catch all
+server.use('*', (req,res)=>{
+    res.send('<h1>Hello, there!</h1>')
+})
+
+server.use( (err, req, res, next) => { //eslint-disable-line
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+server.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}`)
+})
